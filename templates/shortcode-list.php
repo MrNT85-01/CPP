f6ac5ea052d121c9381ee00337e1ed183be0ffa7
MrNT85-01 <maryamjafari5536@gmail.com>
2025-10-11 00:38:41 +0330

    Update shortcode-list.php

--- templates/shortcode-list.php
+++ templates/shortcode-list.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-// --- واکشی تنظیمات ---
 $disable_base_price = get_option('cpp_disable_base_price', 0);
 ?>
 
@@ -13,13 +12,11 @@ $disable_base_price = get_option('cpp_disable_base_price', 0);
                 <th><?php _e('نوع', 'cpp-full'); ?></th>
                 <th><?php _e('واحد', 'cpp-full'); ?></th>
                 <th><?php _e('محل بارگیری', 'cpp-full'); ?></th>
-                
-                <?php if (!$disable_base_price) : ?>
-                <th><?php _e('قیمت پایه', 'cpp-full'); ?></th>
-                <?php endif; ?>
-                
-                <th><?php _e('بازه قیمت', 'cpp-full'); ?></th>
                 <th><?php _e('آخرین بروزرسانی', 'cpp-full'); ?></th>
+                <?php if (!$disable_base_price) : ?>
+                    <th><?php _e('قیمت پایه', 'cpp-full'); ?></th>
+                <?php endif; ?>
+                <th><?php _e('بازه قیمت', 'cpp-full'); ?></th>
                 <th><?php _e('عملیات', 'cpp-full'); ?></th>
             </tr>
         </thead>
@@ -37,11 +34,10 @@ $disable_base_price = get_option('cpp_disable_base_price', 0);
                 <td><?php echo esc_html($product->product_type); ?></td>
                 <td><?php echo esc_html($product->unit); ?></td>
                 <td><?php echo esc_html($product->load_location); ?></td>
-
+                <td><?php echo date_i18n('Y/m/d H:i', strtotime($product->last_updated_at)); ?></td>
                 <?php if (!$disable_base_price) : ?>
-                <td class="cpp-base-price"><?php echo esc_html($product->price); ?></td>
+                    <td class="cpp-base-price"><?php echo esc_html($product->price); ?></td>
                 <?php endif; ?>
-
                 <td class="cpp-price-range">
                     <?php if (!empty($product->min_price) && !empty($product->max_price)) : ?>
                         <?php echo esc_html($product->min_price); ?> - <?php echo esc_html($product->max_price); ?>
@@ -49,7 +45,6 @@ $disable_base_price = get_option('cpp_disable_base_price', 0);
                         <span class="cpp-price-not-set"><?php _e('تماس بگیرید', 'cpp-full'); ?></span>
                     <?php endif; ?>
                 </td>
-                <td><?php echo date_i18n('Y/m/d H:i', strtotime($product->last_updated_at)); ?></td>
                 <td>
                     <button class="cpp-order-btn" data-product-id="<?php echo $product->id; ?>" data-product-name="<?php echo esc_attr($product->name); ?>"><?php _e('ثبت سفارش', 'cpp-full'); ?></button>
                     <button class="cpp-chart-btn" data-product-id="<?php echo $product->id; ?>"><?php _e('نمودار', 'cpp-full'); ?></button>
