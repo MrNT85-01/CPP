c18c498f6503d03f1ce202fd5fab7b9ec3835af1
MrNT85-01 <maryamjafari5536@gmail.com>
2025-10-11 12:45:06 +0330

    Update cpp-settings.php

--- includes/cpp-settings.php
+++ includes/cpp-settings.php
@@ -17,9 +17,8 @@ function cpp_register_settings() {
     
     register_setting('cpp_full_settings_grp', 'cpp_products_per_page');
 
-    // --- شروع تغییر: افزودن تنظیم رنگ دکمه ---
+    // افزودن تنظیم رنگ دکمه
     register_setting('cpp_full_settings_grp', 'cpp_grid_button_color');
-    // --- پایان تغییر ---
 
     // گروه تنظیمات پیامک
     register_setting('cpp_full_settings_grp', 'cpp_sms_service');
