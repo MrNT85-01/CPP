c96877782a4c3cb9813ce97e97d1c5f68c9b1ff3
MrNT85-01 <maryamjafari5536@gmail.com>
2025-10-11 11:48:56 +0330

    Update cpp-settings.php

--- includes/cpp-settings.php
+++ includes/cpp-settings.php
@@ -5,19 +5,19 @@ if (!defined('ABSPATH')) exit;
  * مدیریت تنظیمات افزونه
  */
 
-// هوک برای ثبت تنظیمات در وردپرس
 add_action('admin_init', 'cpp_register_settings');
 
 function cpp_register_settings() {
-    // گروه تنظیمات عمومی ایمیل
+    // گروه تنظیمات عمومی
     register_setting('cpp_full_settings_grp', 'cpp_enable_email');
     register_setting('cpp_full_settings_grp', 'cpp_admin_email');
     register_setting('cpp_full_settings_grp', 'cpp_email_subject_template');
     register_setting('cpp_full_settings_grp', 'cpp_email_body_template');
-    
-    // --- تنظیمات جدید: غیرفعال کردن نمایش قیمت پایه ---
     register_setting('cpp_full_settings_grp', 'cpp_disable_base_price'); 
-    // --------------------------------------------------
+    
+    // --- شروع تغییر: افزودن تنظیم تعداد محصولات ---
+    register_setting('cpp_full_settings_grp', 'cpp_products_per_page');
+    // --- پایان تغییر ---
 
     // گروه تنظیمات پیامک
     register_setting('cpp_full_settings_grp', 'cpp_sms_service');
@@ -26,5 +26,3 @@ function cpp_register_settings() {
     register_setting('cpp_full_settings_grp', 'cpp_admin_phone');
     register_setting('cpp_full_settings_grp', 'cpp_sms_text_template');
 }
-
-// ***توجه: تابع cpp_settings_page() از این فایل حذف شده است تا خطای "Cannot redeclare" رفع شود.*** // این تابع فقط در includes/cpp-admin.php تعریف می‌شود.
