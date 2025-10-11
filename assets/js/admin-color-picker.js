ad7536c62e7afce85ddce30eb62258c9c9752d32
MrNT85-01 <maryamjafari5536@gmail.com>
2025-10-11 12:49:04 +0330

    Create admin-color-picker.js

---
+++ assets/js/admin-color-picker.js
@@ -0,0 +1,7 @@
+(function( $ ) {
+    'use strict';
+    // این کد، انتخاب‌گر رنگ وردپرس را روی فیلد ورودی ما فعال می‌کند.
+    $(function() {
+        $('.cpp-color-picker').wpColorPicker();
+    });
+})( jQuery );
